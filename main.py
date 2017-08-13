b43f39c35aeef18189e46977c81a735362c994c8
Kyle Pitzen <kyle.pitzen@gmail.com>
2017-08-13 10:27:26 -0500

    parsing google finance url payloads

---
+++ main.py
@@ -0,0 +1,24 @@
+'''Main finance scraping control flow module.
+   Currently, we only access the Google Finance API,
+   and receive output as JSON.'''
+
+import argparse
+
+ARG_PARSER = argparse.ArgumentParser()
+SUB_PARSERS = ARG_PARSER.add_subparsers(dest='subparser_name')
+
+FEED_PARSER = SUB_PARSERS.add_parser('feed')
+FEED_PARSER.add_argument('-s', '--stock-ticker')
+FEED_PARSER.add_argument('-t', '--query-type', choices=['option_chain'], default='option_chain')
+
+ARGS = ARG_PARSER.parse_args()
+
+GOOGLE_FINANCE_URL = 'http://www.google.com/finance'
+
+if __name__ == '__main__':
+    if ARGS.subparser_name == 'feed':
+        TICKER_NAME = ARGS.stock_ticker
+        QUERY_TYPE = ARGS.query_type
+        QUERY_PAYLOAD = '?q={}&output=json'.format(TICKER_NAME)
+        URL_PAYLOAD = '/'.join([GOOGLE_FINANCE_URL, ''.join([QUERY_TYPE, QUERY_PAYLOAD])])
+        print(URL_PAYLOAD)
